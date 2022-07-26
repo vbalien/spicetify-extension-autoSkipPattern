965eca1cec93251327b705ea6ac99e4c4b0bf473
Jisu Kim <webmaster@alien.moe>
2022-07-26 14:20:06 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Auto Skip Pattern
+
+Automatically skip songs using regular expression patterns.
+
+## Getting Started
+
+After placing the extension file into correct folder, run following command to install it:
+
+```sh
+spicetify config extensions autoSkipPattern.js
+spicetify apply
+```
+
+## Usage
+
+Enter the pattern on the Spotify client settings page.
+
+## Preview
+
+![Preview](./assets/preview.png)
+
+## Made with Spicetify Creator
+
+- https://github.com/spicetify/spicetify-creator
---
+++ src/app.tsx
@@ -0,0 +1,58 @@
+import { SettingsSection } from 'spcr-settings';
+
+async function setupSettings(): Promise<SettingsSection> {
+  const settings = new SettingsSection('Auto skip pattern', 'settings-test');
+  settings.addToggle('toggle-extension', 'Enable', true);
+  settings.addInput('pattern-title', 'Title pattern(regular expression)', '');
+  settings.addInput('pattern-artist', 'Artist pattern(regular expression)', '');
+
+  await settings.pushSettings();
+  return settings;
+}
+
+function parsePattern(pattern?: string): RegExp | null {
+  if (!pattern) {
+    return null;
+  }
+  const match = pattern.match(new RegExp('^/(.*?)/([gimy]*)$'));
+  if (match) {
+    return new RegExp(match[1], match[2]);
+  } else {
+    return null;
+  }
+}
+
+async function main() {
+  const settings = await setupSettings();
+
+  Spicetify.Player.addEventListener('songchange', () => {
+    if (!settings.getFieldValue('toggle-extension')) {
+      return;
+    }
+    const data = Spicetify.Player.data || Spicetify.Queue;
+    if (!data || !data.track) {
+      return;
+    }
+    const meta = data.track.metadata;
+    if (!meta) {
+      return;
+    }
+
+    const title: string = meta.title ?? '';
+    const artist: string = meta.artist_name ?? '';
+
+    const titlePattern = parsePattern(settings.getFieldValue('pattern-title'));
+    const artistPattern = parsePattern(
+      settings.getFieldValue('pattern-artist')
+    );
+
+    if (
+      (titlePattern && titlePattern.test(title)) ||
+      (artistPattern && artistPattern.test(artist))
+    ) {
+      Spicetify.Player.next();
+    }
+  });
+}
+
+export default main;
